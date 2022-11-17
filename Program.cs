74181ca56bcad0d1aca95702fccee6ce3402fa7f
Oktaviko Rizki <oktaviko.rizki.ft21@mail.umy.ac.id>
2022-11-17 16:19:41 +0700

    add method (class Node)

--- Program.cs
+++ Program.cs
@@ -6,6 +6,17 @@ using System.Threading.Tasks;
 
 namespace DoublyLinkedList
 {
+    class Node
+    {
+        /*Node class represents the node of doubly linked list
+         * It consist of the information part and links to
+         * its seccending and preceding nodes
+         * in terms of next and previous nodes. */
+        public int rollNumber;
+        public string name;
+        public Node next;/*points to the succeeding node*/
+        public Node prev;/*points to the preceeding node*/
+    }
     internal class Program
     {
         static void Main(string[] args)
